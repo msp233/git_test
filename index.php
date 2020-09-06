a9c627bf75d7dd34e12a675a0f2ab673f7569d5a
msp233 <937207392@qq.com>
2020-09-06 18:10:18 +0800

    创建index文件并遍历输出数组

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: spm
+ * Date: 2020/9/6
+ * Time: 6:07 PM
+ */
+
+$arr = [2, 3, 8, 'm', 's' , 'p', '6' , 6];
+foreach ($arr as $k => $v){
+    echo $k . ' => ' . $v . '<br/>';
+}
